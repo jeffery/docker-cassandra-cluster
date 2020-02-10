d9c8835b0d8dafb42b40589568ba90d7b51ce756
Jeffery Fernandez <jefferyfernandez@gmail.com>
2020-02-10 12:54:02 +1100

    Added a whole bunch of commands:
    Command options:
        -b      Bootstrap the cassandra cluster
        -s      Start all the cluster nodes
        -k      Stop all the cluster nodes
        -d      Destroy the cluster :- WARNING -: ALL DATA WILL BE LOST
        -l      Display the cluster status using nodetool
        -r      Display the cluster ring tokens using nodetool
        -c      Launch CQL shell

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 createNetwork() {
   local networkName=$1
@@ -109,6 +109,13 @@ function loadConfig() {
   grep ${varName} ${envFile} | awk -F '=' '{ print $2 }'
 }
 
+PROJECT_PATH=$(dirname "$0")
+ENV_CONFIG_FILE="${PROJECT_PATH}/.env"
+CLUSTER_NAME=$(loadConfig CLUSTER_NAME ${ENV_CONFIG_FILE})
+CLUSTER_SIZE=$(loadConfig CLUSTER_SIZE ${ENV_CONFIG_FILE})
+NODE_MEMORY=$(loadConfig NODE_MEMORY ${ENV_CONFIG_FILE})
+NETWORK_SUBNET_PART=$(loadConfig NETWORK_SUBNET_PART ${ENV_CONFIG_FILE})
+
 function bootStrap() {
   createNetwork ${CLUSTER_NAME} ${NETWORK_SUBNET_PART}
   local networkAddresses=($(getNetworkAddresses ${CLUSTER_SIZE} ${NETWORK_SUBNET_PART} 2))
@@ -127,13 +134,104 @@ function bootStrap() {
   done
 }
 
-PROJECT_PATH=$(dirname "$0")
-ENV_CONFIG_FILE="${PROJECT_PATH}/.env"
-CLUSTER_NAME=$(loadConfig CLUSTER_NAME ${ENV_CONFIG_FILE})
-CLUSTER_SIZE=$(loadConfig CLUSTER_SIZE ${ENV_CONFIG_FILE})
-NODE_MEMORY=$(loadConfig NODE_MEMORY ${ENV_CONFIG_FILE})
-NETWORK_SUBNET_PART=$(loadConfig NETWORK_SUBNET_PART ${ENV_CONFIG_FILE})
+function startCluster() {
+  echo "Starting all cluster nodes"
+  docker start $(docker container ls -qa --filter name=${CLUSTER_NAME}) || echo "Could not start the cluster nodes" 1>&2
+}
 
-bootStrap
+function stopCluster() {
+  echo "Stopping all cluster nodes"
+  docker stop $(docker container ls -qa --filter name=${CLUSTER_NAME}) || echo "Could not stop the cluster nodes" 1>&2
+}
+
+function destroyCluster() {
+  echo "Removing cluster nodes"
+  docker rm $(docker container ls -qa --filter name=${CLUSTER_NAME}) || true
+  echo "Removing cluster network"
+  docker network rm ${CLUSTER_NAME} || true
+}
+
+function clusterStatus() {
+  echo "Displaying Cluster tokens"
+  docker exec -ti "${CLUSTER_NAME}_0" nodetool status || echo "Could not detect status of cluster" 1>&2
+}
+
+function clusterRing() {
+  echo "Fetching Cluster tokens"
+  docker exec -ti "${CLUSTER_NAME}_0" nodetool ring || echo "Could not fetch cluster tokens" 1>&2
+}
+
+function startCQLShell() {
+  echo "Starting Cassandra Query Language shell"
+  docker exec -ti "${CLUSTER_NAME}_0" cqlsh
+}
+
+function usage() {
+  cat << EOF
+${0}
+Command options:
+    -b      Bootstrap the cassandra cluster
+    -s      Start all the cluster nodes
+    -k      Stop all the cluster nodes
+    -d      Destroy the cluster :- WARNING -: ALL DATA WILL BE LOST
+    -l      Display the cluster status using nodetool
+    -r      Display the cluster ring tokens using nodetool
+    -c      Launch CQL shell
+
+EOF
+  exit 1
+}
+
+function invalidOption() {
+    echo "Invalid Command: -$OPTARG" 1>&2
+}
+
+function invalidOptionParameter() {
+    echo "Invalid Option: -$OPTARG" 1>&2
+}
+
+function optionParameterRequiresValue() {
+    echo "Option: -$OPTARG requires an argument" 1>&2
+}
+
+while getopts :hbksdlrc option
+do
+  case "${option}" in
+  h )
+    usage
+    ;;
+  b )
+    bootStrap
+    ;;
+  k )
+    stopCluster
+    ;;
+  s )
+    startCluster
+    ;;
+  d )
+    stopCluster && destroyCluster
+    ;;
+  l )
+    clusterStatus
+    ;;
+  r )
+    clusterRing
+    ;;
+  c )
+    startCQLShell
+    ;;
+  \? )
+    invalidOptionParameter && usage
+    ;;
+  : )
+    optionParameterRequiresValue && usage
+    ;;
+  esac
+done
+
+if [[ $OPTIND -eq 1 ]]; then
+    usage
+fi
 
 exit 0
